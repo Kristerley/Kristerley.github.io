04fe397cadad387387cf7a25f7618c4a339b6bee
Kristerley <37936327+Kristerley@users.noreply.github.com>
2023-09-30 15:02:08 -0400

    init
    Changed icon, added resume, and add home page

---
+++ app/resume/page.tsx
@@ -0,0 +1,71 @@
+export default function Resume(){
+  return (
+    <>
+    <body className="flex flex-col items-center justify-center w-1/2 h-screen">
+    <header className="flex  m-10">
+    <h1 className="text-4xl">Kristerley Ndali</h1>
+    <section className="">
+      <nav>kristerley.work@gmail.com</nav>
+      <nav>(613) 501-9093</nav>
+      <nav>https://github.com/Kristerley</nav>
+      <nav>https://www.linkedin.com/in/kristerley/</nav>
+    </section>
+    </header>
+
+    <div className="">
+    
+    <h3 className="text-2xl mb-2 font-bold">Work Experience</h3>
+    <h5 className="text-xl mb-2 font-bold">Junior Vice President of IT</h5>
+    <h6 className="text-l font-bold">Computer Science Student Association - Ottawa, ON September 2022 to April 2023</h6>
+    <ul>     
+      <li>Develop, test and implement new software programs</li>
+      <li>Design and update software databases. This includes, but is not limited to: Web Sites, Data Communication Processes and User Interfaces</li>
+      <li>Independently install, customize and integrate commercial software packages</li>
+      <li>Resolve complex technical design issues</li>
+      <li>Possess strong problem solving and decision making skills while using good judgment</li>
+    </ul>
+      
+    <h3 className="text-2xl mb-2 font-bold">Education</h3>
+
+    <h6 className="font-bold">Bachelor's Degree in Computer Science</h6>
+    University of Ottawa - Ottawa, ON
+    September 2022 to April 2023 
+    9.2/10 CGPA / A GPA
+
+    <h6 className="font-bold">Bachelor of Computer Science</h6>
+    Concordia University - Montreal, Quebec
+    September 2023 - current
+
+    <div className="flex">
+          
+      <section>
+        <h3 className="text-2xl mb-2 font-bold">Skills</h3>
+          <ul>
+         <li> </li>
+         <li>C++ </li>
+         <li>Python</li>
+         <li>Java</li>
+         <li>Javascript</li>
+         <li>React</li>
+         <li>Node.js</li>
+         <li>Express.js</li>
+         <li>MongoDB</li>
+         <li>Django and Flask</li>
+      </ul>
+      </section>
+      
+    <section>
+      <h3 className="text-2xl mb-2 font-bold">Languages</h3>
+      <ul>
+       <li> </li>       
+       <li>English - Advanced</li> 
+       <li>French - Advanced </li>
+       <li>Russian - Advanced</li>
+      </ul>
+    </section>
+    </div>
+    </div>
+   </body>
+    </>
+  );
+}
